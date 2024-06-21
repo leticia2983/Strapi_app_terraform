0f456f565e388d988c0106e6dee971e72712d960
NevillerBA <123617103+leticia2983@users.noreply.github.com>
2024-06-21 09:38:39 +0400

    added access key secrets variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,15 +13,4 @@ variable "key_name" {
   default     = "aws_login"
 }
 
-variable "aws_access_key" {
-  description = "The AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "The AWS secret key"
-  type        = string
-  sensitive   = true
-}
 
