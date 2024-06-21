5203cc0f7dfcec4b0508a1e4f0b29c7ea30534a7
NevillerBA <123617103+leticia2983@users.noreply.github.com>
2024-06-21 10:15:47 +0400

    added access key secrets variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,33 +25,33 @@ output "instance_public_ip" {
 value = aws_instance.strapi.public_ip
 }
 
-resource "aws_security_group" "strapi_sg" {
-  name = "strapi-sg"
-  description = "Security group for Strapi instance"
-  vpc_id      = var.vpc_id  # Ensure you have a valid VPC ID
-
-  ingress {
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "strapi-sg"
-  }
-}
+#resource "aws_security_group" "strapi_sg" {
+#  name = "strapi-sg"
+#  description = "Security group for Strapi instance"
+#  vpc_id      = var.vpc_id  # Ensure you have a valid VPC ID
+#
+#  ingress {
+#    from_port   = 1337
+#    to_port     = 1337
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  tags = {
+#    Name = "strapi-sg"
+#  }
+#}
