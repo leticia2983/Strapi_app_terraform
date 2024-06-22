62fbe2ebf6fcbfd09fae798da8c230eb34f5e726
NevillerBA <123617103+leticia2983@users.noreply.github.com>
2024-06-22 08:02:27 +0400

    made changes in main.yml, deploy ec2 using terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "instance_type" {
   description = "The instance type to use for the EC2 instance"
-  default     = "t2.medium"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
