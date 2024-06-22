57fddcd672d4d9d719b1ff9c4fe9d000139f744d
NevillerBA <123617103+leticia2983@users.noreply.github.com>
2024-06-22 11:39:15 +0400

    deploy strapi on ec2 StrapiServer_via_terraform_reb

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,7 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "The name of the SSH key pair"
-  default     ="my_aws_login.pem"
+  default     ="my_aws_login"
 }
 #
 #variable "vpc_id" {
