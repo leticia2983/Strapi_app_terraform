620ea4799ea830746c2d7d15cf17b5a34a7315ab
NevillerBA <123617103+leticia2983@users.noreply.github.com>
2024-06-22 06:56:31 +0400

    made changes in main.yml, deploy ec2 using terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,17 +10,17 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "The name of the SSH key pair"
-  default     = "aws_login"
+  default     = "let_login"
 }
-
-variable "vpc_id" {
-  description = "The VPC ID where the security group will be created"
-  type        = string
-  default = "vpc-0bf60a83999950650"
-}
-
-variable "sg_id" {
-  description = "The security_group ID where the security group will be created"
-  type        = string
-  default = "sg-0728132af8b28603a"
-}
+#
+#variable "vpc_id" {
+#  description = "The VPC ID where the security group will be created"
+#  type        = string
+#  default = "vpc-0bf60a83999950650"
+#}
+#
+#variable "sg_id" {
+#  description = "The security_group ID where the security group will be created"
+#  type        = string
+#  default = "sg-0728132af8b28603a"
+#}
