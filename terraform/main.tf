bebf6873a0baebcf4838ac7bfabc5fcfc614652b
NevillerBA <123617103+leticia2983@users.noreply.github.com>
2024-06-22 06:40:50 +0400

    made changes in main.yml, deploy ec2 using terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "strapi" {
   ami           = "ami-0f58b397bc5c1f2e8" # Replace with your preferred AMI ID
   instance_type = var.instance_type
   key_name      = var.key_name
-  vpc_id = var.vpc_id
+
 
 
 
